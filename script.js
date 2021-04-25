d65741d6d5b34c3b23ee4d8d2ead7b0c18610ae3
yavorant <yavorant@gmail.com>
2021-04-25 22:03:28 +0300

    Add changing-color functionality by one color and nice effect by dissapearing

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
 
-const container = document.getElementById("container");
 
-let x = 9, y = 9;
 
-for (let i = 0; i < x; i++) {
-    const row = document.createElement('div');
-    row.classList.add('rows');
-    container.appendChild(row);
 
-    for (let j = 0; j < y; j++) {
-        const divs = document.createElement('div');
-        divs.classList.add('divs');
-        row.appendChild(divs);
-    }
-}    
+function grid(x = 5, y = 10){
+  const container = document.getElementById("container");
+  for (let i = 0; i < x; i++) {
+      const row = document.createElement('div');
+      row.classList.add('rows');
+      container.appendChild(row);
+
+      for (let j = 0; j < y; j++) {
+          const divs = document.createElement('div');
+          divs.classList.add('divs');
+          row.appendChild(divs);
+      }
+  }    
+}  
+
+grid(9, 9);
+
+let elem = document.getElementsByClassName("divs");
+    
+    for (var i = 0; i < 81; i += 1) {
+    
+          elem[i].addEventListener("mouseover", function( event ) {
+          // highlight the mouseover target
+          event.target.style.backgroundColor = "orange";
+        
+          // reset the color after a short delay
+          setTimeout(function() {
+            event.target.style.backgroundColor = "white";
+          }, 1000);
+        }, false);
+    
+      } 
+
+
+alert(element.lenght)
+
